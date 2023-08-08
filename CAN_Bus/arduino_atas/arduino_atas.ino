0a485f3b998d00c3ef28abef295b5d91eeeb9d94
Jihad Rahmawan <jihadrahmawan20@gmail.com>
2023-08-09 06:48:33 +0900

    Add files via upload

---
+++ CAN_Bus/arduino_atas/arduino_atas.ino
@@ -0,0 +1,148 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <SoftwareSerial.h>
+#include "mcp2515.h"
+
+
+struct can_frame canMsg;
+MCP2515 mcp2515(2);
+
+typedef struct {
+  int distance;
+  int strength;
+  boolean receiveComplete;
+} TFmini;
+
+TFmini TFminiDepan = { 0, 0, false };
+TFmini TFminiBawah = { 0, 0, false };
+TFmini TFminiKanan = { 0, 0, false };
+TFmini TFminiKiri = { 0, 0, false };
+
+
+SoftwareSerial portSdepan(3, 4);
+SoftwareSerial portSbawah(5, 6);
+// SoftwareSerial portSkanan(7, 8);
+// SoftwareSerial portSkiri(A1, A0);
+
+
+void getTFminiData(SoftwareSerial* port, TFmini* tfmini) {
+  port->listen();
+  while (tfmini->receiveComplete == false) {
+    static char i = 0;
+    char j = 0;
+    int checksum = 0;
+    static int rx[9];
+    if (port->available()) {
+      rx[i] = port->read();
+      if (rx[0] != 0x59) {
+        i = 0;
+      } else if (i == 1 && rx[1] != 0x59) {
+        i = 0;
+      } else if (i == 8) {
+        for (j = 0; j < 8; j++) {
+          checksum += rx[j];
+        }
+        if (rx[8] == (checksum % 256)) {
+          tfmini->distance = rx[2] + rx[3] * 256;
+          tfmini->strength = rx[4] + rx[5] * 256;
+          tfmini->receiveComplete = true;
+        }
+        i = 0;
+      } else {
+        i++;
+      }
+    }
+  }
+}
+
+
+
+int perintahMagnetNaik = 100;
+int perintahMagnetTurun = 200;
+
+
+
+void setup() {
+  Serial.begin(9600);
+  portSdepan.begin(115200);
+  portSbawah.begin(115200);
+  // portSkanan.begin(115200);
+  // portSkiri.begin(115200);
+
+
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);  //Sets CAN at speed 500KBPS and Clock 8MHz
+  mcp2515.setNormalMode();
+
+
+
+  // put your setup code here, to run once:
+}
+
+void loop() {
+
+  if (Serial.available()) {
+    char a = Serial.read();
+    if (a == 'n') {
+      canMsg.can_id = 0x036;
+      canMsg.can_dlc = 8;
+      canMsg.data[0] = perintahMagnetNaik;
+      canMsg.data[1] = 0x00;
+      canMsg.data[2] = 0x00;
+      canMsg.data[3] = 0x00;
+      canMsg.data[4] = 0x00;
+      canMsg.data[5] = 0x00;
+      canMsg.data[6] = 0x00;
+      canMsg.data[7] = 0x00;
+      mcp2515.sendMessage(&canMsg);
+    }
+    if (a == 't') {
+      canMsg.can_id = 0x036;
+      canMsg.can_dlc = 8;
+      canMsg.data[0] = perintahMagnetTurun;
+      canMsg.data[1] = 0x00;
+      canMsg.data[2] = 0x00;
+      canMsg.data[3] = 0x00;
+      canMsg.data[4] = 0x00;
+      canMsg.data[5] = 0x00;
+      canMsg.data[6] = 0x00;
+      canMsg.data[7] = 0x00;
+      mcp2515.sendMessage(&canMsg);
+    }
+  }
+
+  static unsigned long lastTime = millis();
+  static unsigned int count = 0;
+  static unsigned int frequency = 0;
+
+  getTFminiData(&portSdepan, &TFminiDepan);
+  getTFminiData(&portSbawah, &TFminiBawah);
+
+  if (TFminiDepan.receiveComplete == true && TFminiBawah.receiveComplete == true) {
+    ++count;
+    char buffer[33];
+
+    if (millis() - lastTime > 999) {
+      lastTime = millis();
+      frequency = count;
+      count = 0;
+    }
+
+    Serial.print("Jarak Depan : ");
+    Serial.print(TFminiDepan.distance);
+    Serial.print("cm\t");
+    Serial.print("Jarak Bawah : ");
+    Serial.print(TFminiBawah.distance);
+    Serial.print("cm\t");
+    Serial.print(frequency);  //40~70Hz, It maybe higher if we don't print other thing.
+    Serial.println("Hz");
+
+    // sprintf(buffer, "a%4d%4d%4d%4d",
+    //         TFminiDepan.distance,
+    //         TFminiBawah.distance);
+    // Serial.println(buffer);
+
+    TFminiDepan.receiveComplete = false;
+    TFminiBawah.receiveComplete = false;
+  }
+}
---
+++ CAN_Bus/mekanik_bawah/mekanik_bawah.ino
@@ -0,0 +1,97 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include "mcp2515.h"
+
+struct can_frame can_kirim_arduAtas;
+struct can_frame can_terima_arduAtas;
+MCP2515 mcp2515(2);
+
+const int sharpGP1 = A0;
+const int sharpGP2 = A1;
+const int sharpGP3 = A2;
+const int sharpGP4 = A3;
+int s1 = 0;
+int s2 = 0;
+int s3 = 0;
+int s4 = 0;
+const int PWM_ANGKAT_MAGNET = 6;
+const int PWM_TURUN_MAGNET = 5;
+
+void setup() {
+  pinMode(sharpGP1, INPUT);
+  pinMode(sharpGP2, INPUT);
+  pinMode(sharpGP3, INPUT);
+  pinMode(sharpGP4, INPUT);
+  pinMode(PWM_ANGKAT_MAGNET, OUTPUT);
+  pinMode(PWM_TURUN_MAGNET, OUTPUT);
+  Serial.begin(9600);
+  SPI.begin();
+
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);  //Sets CAN at speed 500KBPS and Clock 8MHz
+  mcp2515.setNormalMode();
+  // put your setup code here, to run once:
+}
+
+
+
+void loop() {
+  s1 = map(analogRead(sharpGP1), 0, 1023, 0, 255);
+  s2 = map(analogRead(sharpGP2), 0, 1023, 0, 255);
+  s3 = map(analogRead(sharpGP3), 0, 1023, 0, 255);
+  s4 = map(analogRead(sharpGP4), 0, 1023, 0, 255);
+
+
+  can_kirim_arduAtas.can_id = 0x036;  //CAN id as 0x036
+  can_kirim_arduAtas.can_dlc = 8;     //CAN data length as 8
+  can_kirim_arduAtas.data[0] = s1;    //Update humidity value in [0]
+  can_kirim_arduAtas.data[1] = s2;    //Update temperature value in [1]
+  can_kirim_arduAtas.data[2] = s3;    //Rest all with 0
+  can_kirim_arduAtas.data[3] = s4;
+  can_kirim_arduAtas.data[4] = 0x00;
+  can_kirim_arduAtas.data[5] = 0x00;
+  can_kirim_arduAtas.data[6] = 0x00;
+  can_kirim_arduAtas.data[7] = 0x00;
+
+  //mcp2515.sendMessage(&can_kirim_arduAtas);
+  //delay(50);
+  // Serial.println(s4);
+  // Serial.println(s2);
+  // Serial.println(s3);
+  // Serial.println(s4);
+
+  if (mcp2515.readMessage(&can_terima_arduAtas) == MCP2515::ERROR_OK) {
+    //if (can_terima_arduAtas.can_id == 0x048) {
+      int perintahmotor = can_terima_arduAtas.data[0];
+      if (perintahmotor == 100) {
+        Serial.println("naik");
+        analogWrite(PWM_ANGKAT_MAGNET, 255);
+        analogWrite(PWM_TURUN_MAGNET, 0);
+      }
+      if (perintahmotor == 200) {
+        Serial.println("turun");
+        analogWrite(PWM_ANGKAT_MAGNET, 0);
+        analogWrite(PWM_TURUN_MAGNET, 255);
+      }
+   // }
+  }
+
+
+  // if (Serial.available()) {
+  //   a = Serial.read();
+  //   if (a == 'a') {
+  //     Serial.println("naik");
+  //     analogWrite(PWM_ANGKAT_MAGNET, 255);
+  //     analogWrite(PWM_TURUN_MAGNET, 0);
+  //   }
+
+  //   if (a == 'b') {
+  //     Serial.println("turun");
+  //     analogWrite(PWM_ANGKAT_MAGNET, 0);
+  //     analogWrite(PWM_TURUN_MAGNET, 255);
+  //   }
+  // }
+
+
+  // put your main code here, to run repeatedly:
+}
