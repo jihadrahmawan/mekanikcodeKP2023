80f2a18e250c2c4df7d887a2b69d4b0a24978a23
Jihad Rahmawan <jihadrahmawan20@gmail.com>
2023-08-10 21:10:15 +0900

    Update mekanik_bawah.ino

--- CAN_Bus/mekanik_bawah/mekanik_bawah.ino
+++ CAN_Bus/mekanik_bawah/mekanik_bawah.ino
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include "mcp2515.h"
+#include <SoftwareSerial.h>
+
+SoftwareSerial com(4, 3);
 
 struct can_frame can_kirim_arduAtas;
 struct can_frame can_terima_arduAtas;
@@ -24,12 +27,13 @@ void setup() {
   pinMode(sharpGP4, INPUT);
   pinMode(PWM_ANGKAT_MAGNET, OUTPUT);
   pinMode(PWM_TURUN_MAGNET, OUTPUT);
-  Serial.begin(9600);
-  SPI.begin();
+  Serial.begin(57600);
+  com.begin(57600);
+  //SPI.begin();
 
-  mcp2515.reset();
-  mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);  //Sets CAN at speed 500KBPS and Clock 8MHz
-  mcp2515.setNormalMode();
+  // mcp2515.reset();
+  // mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);  //Sets CAN at speed 500KBPS and Clock 8MHz
+  // mcp2515.setNormalMode();
   // put your setup code here, to run once:
 }
 
@@ -42,16 +46,16 @@ void loop() {
   s4 = map(analogRead(sharpGP4), 0, 1023, 0, 255);
 
 
-  can_kirim_arduAtas.can_id = 0x036;  //CAN id as 0x036
-  can_kirim_arduAtas.can_dlc = 8;     //CAN data length as 8
-  can_kirim_arduAtas.data[0] = s1;    //Update humidity value in [0]
-  can_kirim_arduAtas.data[1] = s2;    //Update temperature value in [1]
-  can_kirim_arduAtas.data[2] = s3;    //Rest all with 0
-  can_kirim_arduAtas.data[3] = s4;
-  can_kirim_arduAtas.data[4] = 0x00;
-  can_kirim_arduAtas.data[5] = 0x00;
-  can_kirim_arduAtas.data[6] = 0x00;
-  can_kirim_arduAtas.data[7] = 0x00;
+  // can_kirim_arduAtas.can_id = 0x036;  //CAN id as 0x036
+  // can_kirim_arduAtas.can_dlc = 8;     //CAN data length as 8
+  // can_kirim_arduAtas.data[0] = s1;    //Update humidity value in [0]
+  // can_kirim_arduAtas.data[1] = s2;    //Update temperature value in [1]
+  // can_kirim_arduAtas.data[2] = s3;    //Rest all with 0
+  // can_kirim_arduAtas.data[3] = s4;
+  // can_kirim_arduAtas.data[4] = 0x00;
+  // can_kirim_arduAtas.data[5] = 0x00;
+  // can_kirim_arduAtas.data[6] = 0x00;
+  // can_kirim_arduAtas.data[7] = 0x00;
 
   //mcp2515.sendMessage(&can_kirim_arduAtas);
   //delay(50);
@@ -60,38 +64,39 @@ void loop() {
   // Serial.println(s3);
   // Serial.println(s4);
 
-  if (mcp2515.readMessage(&can_terima_arduAtas) == MCP2515::ERROR_OK) {
-    //if (can_terima_arduAtas.can_id == 0x048) {
-      int perintahmotor = can_terima_arduAtas.data[0];
-      if (perintahmotor == 100) {
-        Serial.println("naik");
-        analogWrite(PWM_ANGKAT_MAGNET, 255);
-        analogWrite(PWM_TURUN_MAGNET, 0);
-      }
-      if (perintahmotor == 200) {
-        Serial.println("turun");
-        analogWrite(PWM_ANGKAT_MAGNET, 0);
-        analogWrite(PWM_TURUN_MAGNET, 255);
-      }
-   // }
+  // if (mcp2515.readMessage(&can_terima_arduAtas) == MCP2515::ERROR_OK) {
+  //   //if (can_terima_arduAtas.can_id == 0x048) {
+  //     int perintahmotor = can_terima_arduAtas.data[0];
+  //     if (perintahmotor == 100) {
+  //       Serial.println("naik");
+  //       analogWrite(PWM_ANGKAT_MAGNET, 255);
+  //       analogWrite(PWM_TURUN_MAGNET, 0);
+  //     }
+  //     if (perintahmotor == 200) {
+  //       Serial.println("turun");
+  //       analogWrite(PWM_ANGKAT_MAGNET, 0);
+  //       analogWrite(PWM_TURUN_MAGNET, 255);
+  //     }
+  //  // }
+  // }
+
+//  char a = com.read();
+//     Serial.println(a);
+  if (com.available()) {
+    char a = com.read();
+    Serial.println(a);
+    if (a == 'a') {
+      Serial.println("naik");
+      analogWrite(PWM_ANGKAT_MAGNET, 255);
+      analogWrite(PWM_TURUN_MAGNET, 0);
+    }
+
+    if (a == 'b') {
+      Serial.println("turun");
+      analogWrite(PWM_ANGKAT_MAGNET, 0);
+      analogWrite(PWM_TURUN_MAGNET, 255);
+    }
   }
 
 
-  // if (Serial.available()) {
-  //   a = Serial.read();
-  //   if (a == 'a') {
-  //     Serial.println("naik");
-  //     analogWrite(PWM_ANGKAT_MAGNET, 255);
-  //     analogWrite(PWM_TURUN_MAGNET, 0);
-  //   }
-
-  //   if (a == 'b') {
-  //     Serial.println("turun");
-  //     analogWrite(PWM_ANGKAT_MAGNET, 0);
-  //     analogWrite(PWM_TURUN_MAGNET, 255);
-  //   }
-  // }
-
-
-  // put your main code here, to run repeatedly:
 }
